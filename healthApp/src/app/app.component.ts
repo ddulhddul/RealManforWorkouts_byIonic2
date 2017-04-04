6ac1c63efe116e525790687eafcc1ebe69fc25b7
ddulh <ddulhddul@gmail.com>
2017-04-04 22:13:57 +0900

    tab -> slide menu

--- healthApp/src/app/app.component.ts
+++ healthApp/src/app/app.component.ts
@@ -1,23 +1,35 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, App, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { TabsPage } from '../pages/tabs/tabs';
-
+import { HomePage } from '../pages/home/home';
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  @ViewChild('content') nav
+  rootPage:any = AboutPage;
+  homePage:any = HomePage;
+  aboutPage:any = AboutPage;
+  contactPage:any = ContactPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
+              app: App, public menu: MenuController) {
+    menu.enable(true);
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  openPage(page){
+      this.nav.push(page);
+      this.menu.close();
   }
 }
