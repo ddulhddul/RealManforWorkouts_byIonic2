9850a46ee29657a1dc302675c3b8de41140c1d97
ddulh <ddulhddul@gmail.com>
2017-04-11 22:34:50 +0900

    calendar ion-slides apply

--- healthApp/src/pages/calendar/calendar.ts
+++ healthApp/src/pages/calendar/calendar.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Storage } from '@ionic/storage';
-import { NavController } from 'ionic-angular';
+import { NavController, Slides } from 'ionic-angular';
 import * as Common from '../../common/common';
 
 @Component({
@@ -9,59 +9,94 @@ import * as Common from '../../common/common';
 })
 export class CalendarPage {
 
+    @ViewChild(Slides) slides: Slides;
     calendarFirstDate:Date;
-    calendar:Array<Array<any>>;
-    curDate:Date;
+    calendarArr:any;
 
     constructor(
             public navCtrl: NavController,
             public storage: Storage) {
-        this.showCalendar(null);
+        this.defaultSet(new Date());
     }
 
-    showCalendar(date:Date){
-        this.curDate = date || new Date();
-        this.calendar = [];
-        this.calendarFirstDate = new Date(this.curDate.getFullYear(), this.curDate.getMonth(), 1);
-        this.calendarFirstDate.setDate(
-            this.calendarFirstDate.getDate() - this.calendarFirstDate.getDay()-1
-        );
+    defaultSet(date:Date){
+        this.calendarArr = [];       
+        let date1 = new Date(),date2 = new Date(),date3 = new Date();
+        date1 = new Date(date1.setMonth(date.getMonth()-1));
+        this.calendarArr.push({
+            calendar: this.showCalendar(date1),
+            date: date1
+        });
 
-        this.storage.ready().then(() => {
+        date2 = new Date(date2.setMonth(date.getMonth()));
+        this.calendarArr.push({
+            calendar: this.showCalendar(date2),
+            date: date2
+        });
 
-            calendarLoop:
-            while(true){
-                let week:Array<any> = [];
-                for (let i = 0; i < 7; i++) {
-                    this.calendarFirstDate.setDate(
-                        this.calendarFirstDate.getDate() +1
-                    );
-                    let dateTime = this.calendarFirstDate.getTime();
-                    let yyyymmdd = Common.yyyymmdd(dateTime);
-                    this.storage.get('workout'+yyyymmdd).then((val) => {
-                        let pushObj = {date:null};
-                        if(val) pushObj = JSON.parse(val);
-                        pushObj.date = new Date(dateTime);
-                        week.push(pushObj);
-                    })
-                }
-                this.calendar.push(week);
-                if(this.calendarFirstDate.getFullYear()+(this.calendarFirstDate.getMonth()<10?'0':'')+this.calendarFirstDate.getMonth()
-                    > this.curDate.getFullYear()+(this.curDate.getMonth()<10?'0':'')+this.curDate.getMonth()){
-                    break calendarLoop;
-                }
-            }
+        date3 = new Date(date3.setMonth(date.getMonth()+1));
+        this.calendarArr.push({
+            calendar: this.showCalendar(date3),
+            date: date3
         });
     }
 
+    showCalendar(date:Date){
+        let targetDate = date || new Date();
+        let calendar = [];
+        let targetFirstDate = new Date(targetDate.getFullYear(), targetDate.getMonth(), 1);
+        targetFirstDate.setDate(
+            targetFirstDate.getDate() - targetFirstDate.getDay()-1
+        );
+
+        calendarLoop:
+        while(true){
+            let week:Array<any> = [];
+            for (let i = 0; i < 7; i++) {
+                targetFirstDate.setDate(
+                    targetFirstDate.getDate() +1
+                );
+                let dateTime = targetFirstDate.getTime();
+                let pushObj = {date:null};
+                pushObj.date = new Date(dateTime);
+                week.push(pushObj);
+            }
+            calendar.push(week);
+            if(targetFirstDate.getFullYear()+(targetFirstDate.getMonth()<10?'0':'')+targetFirstDate.getMonth()
+                > targetDate.getFullYear()+(targetDate.getMonth()<10?'0':'')+targetDate.getMonth()){
+                break calendarLoop;
+            }
+        }
+        return calendar;
+    }
+
     prev(param:Date){
-        param.setMonth(param.getMonth()-1);
-        this.showCalendar(param);
+        this.slides.slidePrev(500,true);
     }
     
     next(param:Date){
-        param.setMonth(param.getMonth()+1);
-        this.showCalendar(param);
+        this.slides.slideNext(500,true);
+    }
+
+    slideChanged(swiper:any){
+        
+        let currentIndex = this.slides.getActiveIndex();
+        let toLoadDate = new Date(this.calendarArr[currentIndex].date.getTime());
+        if(currentIndex == this.calendarArr.length-1){
+            let date = new Date(toLoadDate.setMonth(toLoadDate.getMonth()+1));
+            this.calendarArr.push({
+                calendar: this.showCalendar(date),
+                date: date
+            });
+
+        }else if(currentIndex == 0){
+            let date = new Date(toLoadDate.setMonth(toLoadDate.getMonth()-1));
+            this.calendarArr.unshift({
+                calendar: this.showCalendar(date),
+                date: date
+            });
+            this.slides.slideNext(0,false);
+        }
     }
 
 }
